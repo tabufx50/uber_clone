426190f5c9af0a424fdfcf5b7613be4e20e64b41
Richy Jay <tabufx50@hotmail.com>
2021-10-02 07:38:31 -0600

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+// import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Provider store={store}>
+    <NavigationContainer>
+    <SafeAreaProvider>
+      <Stack.Navigator>
+        <Stack.Screen
+            name='HomeScreen'
+            component={HomeScreen}
+            options={{
+              headerShown: false,
+             }}
+         />
+        <Stack.Screen
+            name='MapScreen'
+            component={MapScreen}
+            options={{
+              headerShown: false,
+             }}
+         />
+      </Stack.Navigator>
+
+    {/* <HomeScreen /> */}
+    </SafeAreaProvider>
+
+    </NavigationContainer>
+    {/* <View style={styles.container}>
+      <Text>Let's build Uber!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> */}
+
+    </Provider>
   );
 }
 
